c727326adeab3254adcd90049cb23af04715fccc
Naimar1 <naiglyme1@gmail.com>
2019-02-26 12:34:28 +0200

    Create all file i need to use

---
+++ app/main/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+class ReviewForm(FlaskForm):
+
+    title = StringField('Review title',validators=[Required()])
+    review = TextAreaField('Movie review')
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='7854ce99b646526f11604daf32dc1202'
+export SECRET_KEY='0788504294ma0788896659pa'
+
+export MAIL_USERNAME='naiglyme1@gmail.com'
+export MAIL_PASSWORD='nabeglyme5050!$'
+python3.6 manage.py server
