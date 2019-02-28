f461d73e0ae0c8814343faa12f5b0ffe21b4726b
Naimar1 <naiglyme1@gmail.com>
2019-02-28 21:38:46 +0200

    Put all test for user,coments and pitches

--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -1,19 +1,36 @@
-import unittest
-from app.models import Movie
+# import unittest
+# from app.models import Movie
 
-class MovieTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the Movie class
-    '''
+# class MovieTest(unittest.TestCase):
+#     '''
+#     Test Class to test the behaviour of the Movie class
+#     '''
+
+#     def setUp(self):
+#         '''
+#         Set up method that will run before every Test
+#         '''
+#         self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_movie,Movie))
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+
+import unittest
+from app.models import Pitch
+
+class PitchModelTest(unittest.TestCase):
 
     def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+        self.new_pitch = Pitch(id = 1, title = 'What Makes a Song Sad', pitch_content = 'Where does sad music get its sadness from? And whom should you ask—a composer or a cognitive psychologist?', category = 'Pickup Lines', upvote = 10, downvote = 12, author = 'DANIEL WATTENBERG'
+)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_movie,Movie))
+        self.assertTrue(isinstance(self.new_pitch, Pitch))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all())>0)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,17 +1,38 @@
+# import unittest
+# from app.models import User
+
+# class UserModelTest(unittest.TestCase):
+
+#     def setUp(self):
+#         self.new_user = User(password = 'banana')
+
+#     def test_password_setter(self):
+#         self.assertTrue(self.new_user.pass_secure is not None)
+
+#     def test_no_access_password(self):
+#         with self.assertRaises(AttributeError):
+#             self.new_user.password
+
+#     def test_password_verification(self):
+#         self.assertTrue(self.new_user.verify_password('banana'))
+
 import unittest
 from app.models import User
 
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User(password = 'banana')
+        self.new_user = User(id = 1, password = '123', username = 'wecode', email="naiglyme1@gmail.com", bio = 'Never give up', profile_pic_path='/photos/login.jpg')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_user, User))
 
     def test_password_setter(self):
-        self.assertTrue(self.new_user.pass_secure is not None)
+        self.assertTrue(self.new_user.password_hash is not None)
 
     def test_no_access_password(self):
         with self.assertRaises(AttributeError):
             self.new_user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('banana'))
+        self.assertTrue(self.new_user.verify_password('123'))
