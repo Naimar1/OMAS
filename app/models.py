569d4e003283c351854222e499cd722143dfc552
Naimar1 <naiglyme1@gmail.com>
2019-03-01 17:53:53 +0200

    Create model post in my models file and and save them in the database

--- app/models.py
+++ app/models.py
@@ -17,8 +17,9 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
+
     comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
-    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    posts = db.relationship('Post',backref = 'user',lazy = "dynamic")
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -35,54 +36,34 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-class Pitch(db.Model):
-    __tablename__ = 'pitches'
+class Post(db.Model):
+    __tablename__ = 'posts'
 
     id = db.Column(db.Integer,primary_key = True)
-    pitch_title = db.Column(db.String)
-    pitch_content = db.Column(db.String(255))
-    category = db.Column(db.String)
+    post_title = db.Column(db.String)
+    post_content = db.Column(db.String(255))
     posted = db.Column(db.DateTime,default=datetime.utcnow)
-    like = db.Column(db.Integer)
-    dislike = db.Column(db.Integer)
+
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
     comments = db.relationship('Comment',backref = 'pitch',lazy="dynamic") 
 
-    def save_pitch(self):
+    def save_post(self):
         db.session.add(self)
         db.session.commit()
 
-    @classmethod
-    def get_pitches(cls,category):
-        pitches = Pitch.query.filter_by(category=category).all()
-        return pitches
+    def get_post(id):
+        post = Post.query.filter_by(id=id).first()
+        return post
 
-    @classmethod
-    def get_pitch(cls,id):
-        pitch = Pitch.query.filter_by(id=id).first()
-
-        return pitch
-
-    @classmethod
-    def count_pitches(cls,uname):
-        user = User.query.filter_by(username=uname).first()
-        pitches = Pitch.query.filter_by(user_id=user.id).all()
-
-        pitches_count = 0
-        for pitch in pitches:
-            pitches_count += 1
-
-        return pitches_count
-
+    
 class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer,primary_key = True)
     comment = db.Column(db.String(255))
-    comment_writer = db.Column(db.String)
     posted = db.Column(db.DateTime,default=datetime.utcnow)
     
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    post = db.Column(db.Integer, db.ForeignKey('posts.id'))
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
     def save_comment(self):
